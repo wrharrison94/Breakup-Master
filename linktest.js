5842e8225de95991ced0e4cfe1bdea6a502b9534
Unknown <wrharrison94@gmail.com>
2017-12-05 15:37:52 -0500

    first

---
+++ linktest.js
@@ -0,0 +1,24 @@
+
+$(document).keypress(function(e) {
+    if (e.which == 49) {
+        $("#link1")[0].click();
+    }
+});
+
+$(document).keypress(function(e) {
+    if (e.which == 50) {
+        $("#link2")[0].click();
+    }
+});
+
+$(document).keypress(function(e) {
+    if (e.which == 51) {
+        $("#link3")[0].click();
+    }
+});
+
+$(document).keypress(function(e) {
+    if (e.which == 13) {
+        $("#linkenter")[0].click();
+    }
+});
